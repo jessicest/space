3d88a82b210f89b72d42b6cbb3e55b169612a347
Jessica Knight <corence@gmail.com>
2023-12-23 16:19:18 +1100

    fixed cargo info for multiple types! probably still works

--- Cargo View/Program.cs
+++ Cargo View/Program.cs
@@ -28,54 +28,74 @@ namespace IngameScript {
         }
 
         public void Main(string argument, UpdateType updateSource) {
-            Reinit();
+            if (_counter <= 0) {
+                Reinit();
+            }
+            _counter -= 1;
             Show(CountTypes());
         }
 
         private void Reinit() {
-            if (_counter <= 0) {
-                _counter = 1000;
-                _cargos.Clear();
-                GridTerminalSystem.GetBlocksOfType(_cargos, block => block.IsSameConstructAs(Me));
-                _lcds.Clear();
-                GridTerminalSystem.GetBlocksOfType(_lcds,
-                    block => block.IsSameConstructAs(Me) && block.CustomName.StartsWith("Cargo Info:"));
-            }
-            _counter -= 1;
+            _counter = 1000;
+            _cargos.Clear();
+            GridTerminalSystem.GetBlocksOfType(_cargos, block => block.IsSameConstructAs(Me));
+            _lcds.Clear();
+            GridTerminalSystem.GetBlocksOfType(_lcds,
+                block => block.IsSameConstructAs(Me) && block.CustomName.StartsWith("Cargo Info:"));
         }
 
-        private SortedDictionary<MyItemType, MyFixedPoint> CountTypes() {
-            SortedDictionary<MyItemType, MyFixedPoint> cargoCounts = new SortedDictionary<MyItemType, MyFixedPoint>();
+        private Dictionary<string, SortedDictionary<string, MyFixedPoint>> CountTypes() {
+            Dictionary<string, SortedDictionary<string, MyFixedPoint>> cargoCounts = new Dictionary<string, SortedDictionary<string, MyFixedPoint>>();
             List<MyInventoryItem> items = new List<MyInventoryItem>();
 
             foreach (IMyCargoContainer cargo in _cargos) {
-                cargo.GetInventory(0).GetItems(items);
-                foreach (MyInventoryItem item in items) {
-                    if (!cargoCounts.ContainsKey(item.Type)) {
-                        cargoCounts.Add(item.Type, 0);
+                if (!cargo.IsFunctional) {
+                    continue;
+                }
+
+                for (int i = 0; i < cargo.InventoryCount; ++i) {
+                    items.Clear();
+                    cargo.GetInventory(i).GetItems(items);
+                    foreach (MyInventoryItem item in items) {
+                        SortedDictionary<string, MyFixedPoint> subtypeCounts;
+                        if (!cargoCounts.TryGetValue(item.Type.TypeId, out subtypeCounts)) {
+                            subtypeCounts = new SortedDictionary<string, MyFixedPoint>();
+                            cargoCounts.Add(item.Type.TypeId, new SortedDictionary<string, MyFixedPoint>());
+                        }
+
+                        if (!subtypeCounts.ContainsKey(item.Type.SubtypeId)) {
+                            subtypeCounts.Add(item.Type.SubtypeId, item.Amount);
+                        } else {
+                            subtypeCounts[item.Type.SubtypeId] += item.Amount;
+                        }
                     }
-                    cargoCounts[item.Type] += item.Amount;
                 }
             }
 
             return cargoCounts;
         }
 
-        private void Show(SortedDictionary<MyItemType, MyFixedPoint> typeCounts) {
+        private void Show(Dictionary<string, SortedDictionary<string, MyFixedPoint>> typeCounts) {
             Dictionary<string, string> outputs = new Dictionary<string, string>();
 
-            foreach (KeyValuePair<MyItemType, MyFixedPoint> typeCount in typeCounts) {
-                string typeId = typeCount.Key.TypeId;
-                if (!outputs.ContainsKey(typeId)) {
-                    outputs.Add(typeId.Substring(typeId.IndexOf("_") + 1), "");
+            foreach (KeyValuePair<string, SortedDictionary<string, MyFixedPoint>> subtypeCounts in typeCounts) {
+                string typeId = subtypeCounts.Key.Substring(subtypeCounts.Key.IndexOf("_") + 1);
+                string s = typeId + "\n\n";
+
+                foreach (KeyValuePair<string, MyFixedPoint> subtypeCount in subtypeCounts.Value) {
+                    s += subtypeCount.Key + ": " + subtypeCount.Value.ToIntSafe() + "\n";
                 }
 
-                outputs[typeCount.Key.TypeId] += typeCount.Key.SubtypeId + ": " + typeCount.Value + "\n";
+                outputs.Add(typeId, s);
             }
 
             Echo(String.Join(", ", outputs.Keys.ToArray()));
 
             foreach (IMyTextPanel lcd in _lcds) {
+                if (!lcd.IsFunctional) {
+                    continue;
+                }
+
                 string lcdName = lcd.CustomName;
                 string targetNamePrefix = lcdName.Substring(lcdName.IndexOf(':') + 1);
                 string targetName = targetNamePrefix.Substring(0, targetNamePrefix.IndexOf(':'));
