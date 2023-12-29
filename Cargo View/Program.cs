52f36f1c8851f965bbd39f944ba89168789af58a
Jessica Knight <corence@gmail.com>
2023-12-29 21:44:20 +1100

    add useless/fun GridStats. not tested

--- Cargo View/Program.cs
+++ Cargo View/Program.cs
@@ -15,7 +15,13 @@ namespace IngameScript {
         private readonly List<IMyTextPanel> _lcds = new List<IMyTextPanel>();
         private readonly List<IMyTerminalBlock> _cargos = new List<IMyTerminalBlock>();
         private readonly List<IMyAssembler> _assemblers = new List<IMyAssembler>();
+        private readonly List<IMyGasTank> _oxygen_tanks= new List<IMyGasTank>();
+        private readonly List<IMyGasTank> _hydrogen_tanks = new List<IMyGasTank>();
+        private readonly List<IMyBatteryBlock> _batteries = new List<IMyBatteryBlock>();
+        private readonly List<IMyLargeTurretBase> _turrets = new List<IMyLargeTurretBase>();
+        private readonly List<IMyShipConnector> _connectors = new List<IMyShipConnector>();
         private IMyCargoContainer _recycle_bin;
+        private readonly DateTime _start_time;
         private int _counter = 0;
         private bool _echoed = false;
 
@@ -28,6 +34,7 @@ namespace IngameScript {
         }
 
         public Program() {
+            _start_time = DateTime.Now;
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
         }
 
@@ -38,6 +45,7 @@ namespace IngameScript {
             _counter -= 1;
 
             Dictionary<string, SortedDictionary<string, MyFixedPoint>> cargoCounts = new Dictionary<string, SortedDictionary<string, MyFixedPoint>>();
+            CollectGridStats(cargoCounts);
             CountCargoTypes(cargoCounts);
             CountProduction(cargoCounts, true);
             Show(cargoCounts);
@@ -50,15 +58,91 @@ namespace IngameScript {
 
             _cargos.Clear();
             GridTerminalSystem.GetBlocksOfType(_cargos,
-                block => block.IsSameConstructAs(Me) && block.HasInventory);
+                block => block.IsSameConstructAs(Me)
+                && block.HasInventory && block.IsWorking);
 
             _assemblers.Clear();
             GridTerminalSystem.GetBlocksOfType(_assemblers,
-                block => block.IsSameConstructAs(Me));
+                block => block.IsSameConstructAs(Me)
+                && block.IsWorking);
 
             _lcds.Clear();
             GridTerminalSystem.GetBlocksOfType(_lcds,
-                block => block.IsSameConstructAs(Me) && block.CustomName.StartsWith("Cargo Info:"));
+                block => block.IsSameConstructAs(Me)
+                && block.CustomName.StartsWith("Cargo Info:"));
+
+            _oxygen_tanks.Clear();
+            GridTerminalSystem.GetBlocksOfType(_oxygen_tanks,
+                block => block.IsSameConstructAs(Me)
+                && block.BlockDefinition.SubtypeName.Contains("Oxygen"));
+
+            _hydrogen_tanks.Clear();
+            GridTerminalSystem.GetBlocksOfType(_hydrogen_tanks,
+                block => block.IsSameConstructAs(Me)
+                && block.BlockDefinition.SubtypeName.Contains("Hydrogen"));
+
+            _batteries.Clear();
+            GridTerminalSystem.GetBlocksOfType(_batteries,
+                block => block.IsSameConstructAs(Me)
+                && block.IsWorking);
+
+            _turrets.Clear();
+            GridTerminalSystem.GetBlocksOfType(_turrets,
+                block => block.IsSameConstructAs(Me)
+                && block.IsWorking);
+
+            _connectors.Clear();
+            GridTerminalSystem.GetBlocksOfType(_connectors,
+                block => block.IsSameConstructAs(Me)
+                && block.IsWorking);
+        }
+
+        MyFixedPoint CountFullness<T>(List<T> blocks, Func<T, double> getCurrent, Func<T, double> getMax) where T : IMyTerminalBlock {
+            return CountFullness(blocks, a => (float)getCurrent(a), a => (float)getMax(a));
+        }
+
+        MyFixedPoint CountFullness<T>(List<T> blocks, Func<T, float> getCurrent, Func<T, float> getMax) where T: IMyTerminalBlock {
+            float current = 0;
+            float max = 0;
+
+            foreach (T b in blocks.Where(b => b.IsWorking)) {
+                current += getCurrent(b);
+                max += getMax(b);
+            }
+
+            if (max > 0) {
+                return (MyFixedPoint)(current / max);
+            } else {
+                return MyFixedPoint.Zero;
+            }
+        }
+
+        float QueryInventories(IEnumerable<IMyTerminalBlock> blocks, Func<IMyInventory, MyFixedPoint> f) {
+            float result = 0;
+
+            foreach(IMyTerminalBlock block in blocks.Where(b => b.IsWorking)) {
+                for (int i = 0; i < block.InventoryCount; ++i) {
+                    result += (float)f(block.GetInventory(i));
+                }
+            }
+
+            return result;
+        }
+
+        private void CollectGridStats(Dictionary<string, SortedDictionary<string, MyFixedPoint>> cargoCounts) {
+            double elapsedTime = (DateTime.Now - _start_time).TotalDays;
+            SortedDictionary<string, MyFixedPoint> counts = new SortedDictionary<string, MyFixedPoint>();
+            counts.Add("Battery input %:", 100 * CountFullness(_batteries, b => b.CurrentInput, b => b.MaxInput));
+            counts.Add("Battery output %:", 100 * CountFullness(_batteries, b => b.CurrentOutput, b => b.MaxOutput));
+            counts.Add("Battery stored %:", 100 * CountFullness(_batteries, b => b.CurrentStoredPower, b => b.MaxStoredPower));
+            counts.Add("Cargo mass (t):", (MyFixedPoint)((int)QueryInventories(_cargos, inv => inv.CurrentMass) / 1000));
+            counts.Add("Script uptime:", (MyFixedPoint)(DateTime.Now - _start_time).TotalDays);
+            counts.Add("Docked ships:", _connectors.Where(c => c.IsWorking && c.IsConnected).Count());
+            counts.Add("Hydrogen %:", 100 * CountFullness(_hydrogen_tanks, b => b.FilledRatio * b.Capacity, b => b.Capacity));
+            counts.Add("Oxygen %:", 100 * CountFullness(_oxygen_tanks, b => b.FilledRatio * b.Capacity, b => b.Capacity));
+            counts.Add("Turrets, idle:", _turrets.Where(t => t.IsWorking && !t.HasTarget).Count());
+            counts.Add("Turrets, targeting:", _turrets.Where(t => t.IsWorking && t.HasTarget).Count());
+            cargoCounts.Add("GridStats", counts);
         }
 
         private void CountCargoTypes(Dictionary<string, SortedDictionary<string, MyFixedPoint>> cargoCounts) {
