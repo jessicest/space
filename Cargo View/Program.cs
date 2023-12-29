5236026a2d86bb74646daa616eff5c3feb32801e
Jessica Knight <corence@gmail.com>
2023-12-30 10:07:41 +1100

    add idle assemblers to gridtidbits

--- Cargo View/Program.cs
+++ Cargo View/Program.cs
@@ -156,17 +156,18 @@ namespace IngameScript {
 
             double elapsedTime = (DateTime.Now - _start_time).TotalDays;
             s += "Battery input: " + (100 * CountFullness(_batteries, b => b.CurrentInput, b => b.MaxInput)) + "%\n";
-            s += "Battery output:" + (100 * CountFullness(_batteries, b => b.CurrentOutput, b => b.MaxOutput)) + "%\n";
+            s += "Battery output: " + (100 * CountFullness(_batteries, b => b.CurrentOutput, b => b.MaxOutput)) + "%\n";
             s += "Battery stored: " + (100 * CountFullness(_batteries, b => b.CurrentStoredPower, b => b.MaxStoredPower)) + "%\n";
-            s += "Cargo mass:" + ((int)QueryInventories(_cargos, inv => inv.CurrentMass) / 1000) + "t\n";
+            s += "Cargo mass: " + ((int)QueryInventories(_cargos, inv => inv.CurrentMass) / 1000) + "t\n";
             s += "Docked ships: " + _connectors.Where(c => c.IsWorking && c.IsConnected).Count() + "\n";
             s += "Hydrogen:" + (100 * CountFullness(_hydrogen_tanks, b => b.FilledRatio * b.Capacity, b => b.Capacity)) + "%\n";
-            s += "Oxygen:" + (100 * CountFullness(_oxygen_tanks, b => b.FilledRatio * b.Capacity, b => b.Capacity)) + "%\n";
+            s += "Idle assembers: " + _assemblers.Where(a => a.IsWorking && a.IsQueueEmpty).Count() + "\n";
+            s += "Oxygen: " + (100 * CountFullness(_oxygen_tanks, b => b.FilledRatio * b.Capacity, b => b.Capacity)) + "%\n";
             s += "Script activity: " + ((DateTime.Now.ToString())) + "\n";
             s += "Script uptime: " + ((DateTime.Now - _start_time).TotalDays) + " days\n";
             s += "Script version: v" + _version + "\n";
-            s += "Turrets, idle:" + _turrets.Where(t => t.IsWorking && !t.HasTarget).Count() + "\n";
-            s += "Turrets, targeting:" + _turrets.Where(t => t.IsWorking && t.HasTarget).Count() + "\n";
+            s += "Turrets, idle: " + _turrets.Where(t => t.IsWorking && !t.HasTarget).Count() + "\n";
+            s += "Turrets, targeting: " + _turrets.Where(t => t.IsWorking && t.HasTarget).Count() + "\n";
 
             infos.Add("GridTidbits", s);
         }
